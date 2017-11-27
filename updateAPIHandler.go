c2caf59b593ad184ebfd14738b0b25bf5ba21a22
stephenhu <epynonymous@outlook.com>
2017-11-27 18:47:28 +0800

    complete directory path

--- updateAPIHandler.go
+++ updateAPIHandler.go
@@ -109,7 +109,7 @@ func installUpdate(file string) bool {
 	
 	glbs.SetNamespace(UPDATES)
 
-	deb := fmt.Sprintf("sudo dpkg -i %s", file)
+	deb := fmt.Sprintf("dpkg -i /home/mboard/bin/%s", file)
 	
 	out, err := exec.Command(deb).Output()
 
